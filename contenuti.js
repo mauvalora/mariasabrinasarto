ecddaeed1cf861edb98871ee80320b03d2fd99d4
mauvalora <mauro.valorani@uniroma1.it>
2026-07-21 11:19:08 +0200

    inseriti eventi in agenda

--- contenuti.js
+++ contenuti.js
@@ -6,16 +6,202 @@
 window.SITE_DATA = {
   "agenda": [
     {
+      "giorno": "9",
+      "mese": "Giugno",
+      "titolo": "Incontro con la Scuola di Ingegneria Aerospaziale",
+      "dettagli": ""
+    },
+    {
+      "giorno": "16",
+      "mese": "Giugno",
+      "titolo": "Incontro con Dipartimento di Ingegneria Elettronica e Telecomunicazioni",
+      "dettagli": ""
+    },
+    {
+      "giorno": "24",
+      "mese": "Giugno",
+      "titolo": "Incontro con Dipartimento di Ingegneria Elettrica ed Energetica",
+      "dettagli": ""
+    },
+    {
+      "giorno": "25",
+      "mese": "Giugno",
+      "titolo": "Incontro con Dipartimento Scienze mediche e cardiovascolari",
+      "dettagli": ""
+    },
+    {
+      "giorno": "25",
+      "mese": "Giugno",
+      "titolo": "Incontro con Dipartimento Scienze di Base applicate all'Ingegneria",
+      "dettagli": ""
+    },
+    {
+      "giorno": "26",
+      "mese": "Giugno",
+      "titolo": "Incontro con Dipartimento Ingegneria Informatica",
+      "dettagli": ""
+    },
+    {
+      "giorno": "26",
+      "mese": "Giugno",
+      "titolo": "Incontro con Dipartimento Scienze Biochimiche Rossi-Fanelli",
+      "dettagli": ""
+    },
+    {
+      "giorno": "26",
+      "mese": "Giugno",
+      "titolo": "Incontro con Dipartimento di Fisiologia e Farmacologia",
+      "dettagli": ""
+    },
+    {
+      "giorno": "1",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento MeMoTEF",
+      "dettagli": ""
+    },
+    {
+      "giorno": "2",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento PDTA",
+      "dettagli": ""
+    },
+    {
+      "giorno": "3",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Medicina Molecolare",
+      "dettagli": ""
+    },
+    {
+      "giorno": "3",
+      "mese": "Luglio",
+      "titolo": "Incontro con Area Terza e Quarta Missione",
+      "dettagli": ""
+    },
+    {
+      "giorno": "7",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Diritto ed Economia di Impresa",
+      "dettagli": ""
+    },
+    {
+      "giorno": "8",
+      "mese": "Luglio",
+      "titolo": "Incontro con Coordinamento Responsabili Amministrativi",
+      "dettagli": ""
+    },
+    {
+      "giorno": "8",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Chimica e Tecnologie del Farmaco",
+      "dettagli": ""
+    },
+    {
+      "giorno": "9",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Medicina Sperimentale",
+      "dettagli": ""
+    },
+    {
+      "giorno": "10",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Studi Europei, Americani e Interculturali",
+      "dettagli": ""
+    },
+    {
+      "giorno": "10",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento SAIMLAL",
+      "dettagli": ""
+    },
+    {
+      "giorno": "10",
+      "mese": "Luglio",
+      "titolo": "Incontro con Coordinamento dei ricercatori di area medica - macroarea C",
+      "dettagli": ""
+    },
+    {
       "giorno": "13",
       "mese": "Luglio",
       "titolo": "Aula Magna, ore 10:30. Presentazione dei programmi elettorali",
       "dettagli": "Il Decano del Corpo Accademico ha fissato con il Decreto n. 1400/2026 gli incontri pubblici nelle seguenti date e orari:\n\n- Lunedì 13 luglio 2026, ore 10:30, primo incontro, presentazione dei programmi elettorali;\n\n- Martedì 15 settembre 2026, ore 10:00, secondo incontro, dibattito pubblico;\n\n- Venerdì 25 settembre 2026, ore 10:00, terzo incontro, appello agli elettori.\n\nCon il medesimo provvedimento, il Decano ha, altresì, adottato le allegate \"Linee guida per la regolamentazione degli incontri pubblici - Elezioni del/della Rettore/Rettrice 2026-2032”, approvate dal Comitato dei Garanti nella seduta tenutasi in data 1° luglio 2026.\n\nInoltre, si è prevista l’attivazione dalle ore 16:00 del 10.07.2026 sino alle ore 23:59 del 04.10.2026 della apposita sezione del portale di voto dedicata al “Forum Sapienza”, fruibile secondo le modalità che saranno pubblicate sulla pagina web Sapienza dedicata alle elezioni."
     },
     {
+      "giorno": "13",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Organi di Senso",
+      "dettagli": ""
+    },
+    {
+      "giorno": "14",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Storia, Antropologia, Religioni, Arte, Spettacolo",
+      "dettagli": ""
+    },
+    {
+      "giorno": "14",
+      "mese": "Luglio",
+      "titolo": "Incontro con Coordinamento dei Professori Associati di Ateneo",
+      "dettagli": ""
+    },
+    {
+      "giorno": "14",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Comunicazione e Ricerca Sociale",
+      "dettagli": ""
+    },
+    {
+      "giorno": "15",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Ingegneria Strutturale e Geotecnica",
+      "dettagli": ""
+    },
+    {
+      "giorno": "15",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Sanità Pubblica e Malattie Infettive",
+      "dettagli": ""
+    },
+    {
+      "giorno": "15",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento di Architettura e Progetto",
+      "dettagli": ""
+    },
+    {
+      "giorno": "16",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento di Ingegneria Civile, Edile, Ambientale",
+      "dettagli": ""
+    },
+    {
+      "giorno": "16",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento di Scienze Radiologiche, Oncologiche e Anatomo-Patologiche",
+      "dettagli": ""
+    },
+    {
       "giorno": "17",
-      "mese": "luglio",
+      "mese": "Luglio",
       "titolo": "Facoltà ICI, ore 15:00. Incontro di presentazione dei candidati alla carica rettorale",
       "dettagli": "Venerdì 17 luglio, con inizio alle ore 15:00, in aula 1 presso la sede di San Pietro in Vincoli — via Eudossiana n. 18 —  si terrà un incontro di presentazione alla comunità ICI dei candidati alla carica di Rettrice/Rettore della Sapienza per il sessennio 2026-2032.\n\nI 5 candidati avranno a disposizione 15 minuti per presentarsi, dopo i quali seguiranno le loro risposte — limitate a due minuti per risposta — ad un insieme di domande prese da una lista che verrà comunicata in anticipo ai candidati.\n\nChiunque voglia contribuire alla formazione della lista di domande è pregato di scrivere alla presidenza formulando in modo succinto la questione che vuole sia affrontata nel dibattito."
+    },
+    {
+      "giorno": "17",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento di Storia, Disegno e Restauro dell'Architettura",
+      "dettagli": ""
+    },
+    {
+      "giorno": "17",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Economia e Diritto",
+      "dettagli": ""
+    },
+    {
+      "giorno": "20",
+      "mese": "Luglio",
+      "titolo": "Incontro con Dipartimento Scienze Statistiche",
+      "dettagli": ""
     },
     {
       "giorno": "15",
@@ -25,11 +211,24 @@ window.SITE_DATA = {
     },
     {
       "giorno": "25",
-      "mese": "settembre",
+      "mese": "Settembre",
       "titolo": "Aula Magna, ore 10:00. Appello agli elettori",
       "dettagli": ""
     }
   ],
   "faq": [],
-  "stampa": []
+  "stampa": [
+    {
+      "testata": "Testata",
+      "titolo": "Titolo dell'articolo o dell'intervista",
+      "data": "gg mese 2026",
+      "url": ""
+    },
+    {
+      "testata": "Testata",
+      "titolo": "Titolo dell'articolo o dell'intervista",
+      "data": "gg mese 2026",
+      "url": ""
+    }
+  ]
 };
